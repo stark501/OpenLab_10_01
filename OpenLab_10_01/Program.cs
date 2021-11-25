55b5fe7e49d85e21a15c1fcd6b2be2cf0c4d1815
stark501 <mkudlacej@gmail.com>
2021-11-25 20:57:32 +0100

    Add project files.

---
+++ OpenLab_10_01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OpenLab_10_01
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            Book Lotr = new Book();
+            Lotr.title = "LOTR";
+            Console.WriteLine(Lotr.title);
+        }
+        class Book
+        {
+            public string title;
+            private int pages;
+        }
+
+
+    }
+}
