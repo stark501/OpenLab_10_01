bfab00b779e0c1c198332eddd2f03f483e0a5380
stark501 <mkudlacej@gmail.com>
2021-12-01 19:59:00 +0100

    OpenLab10.02

--- OpenLab_10_01/Program.cs
+++ OpenLab_10_01/Program.cs
@@ -5,18 +5,38 @@ namespace OpenLab_10_01
     class Program
     {
 
-        static void Main()
+        static void Main(string[] args)
         {
             Book Lotr = new Book();
-            Lotr.title = "LOTR";
-            Console.WriteLine(Lotr.title);
+            Lotr.Title = ("Janko Kral");
+            Lotr.Pages = ("352");
+            Lotr.Category = ("Romantic");
+            Lotr.Autor = ("Marina");
+            Lotr.ReleaseDate = ("32.7.2004");
         }
         class Book
         {
-            public string title;
+            private string title;
+            private int pages;
+            private string category;
+            private string author;
+            private int releaseDate;
+
+            public void Title(string newTitle) { title = newTitle; }
+            public void Pages(int newPages) { pages = newPages; }
+            public void Category(string newCategory) { category = newCategory; }
+            public void Autor(string newAutor) { autor = newAuthor; }
+            public void ReleaseDate(int newReleaseDate) { releaseDate = newReleaseDate; }
             private int pages;
         }
-
+        public void Write()
+        {
+            Console.WriteLine(title);
+            Console.WriteLine(pages);
+            Console.WriteLine(category);
+            Console.WriteLine(author);
+            Console.WriteLine(releaseDate);
+        }
 
     }
 }
