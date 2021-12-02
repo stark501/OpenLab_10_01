b5bf7596a4921637af3b2638112c6332ae0752ef
stark501 <mkudlacej@gmail.com>
2021-12-02 12:04:55 +0100

    OpenLab02-OPRAVA

--- OpenLab_10_01/Program.cs
+++ OpenLab_10_01/Program.cs
@@ -8,35 +8,36 @@ namespace OpenLab_10_01
         static void Main(string[] args)
         {
             Book Lotr = new Book();
-            Lotr.Title = ("Janko Kral");
-            Lotr.Pages = ("352");
-            Lotr.Category = ("Romantic");
-            Lotr.Autor = ("Marina");
-            Lotr.ReleaseDate = ("32.7.2004");
+            Lotr.SetTitle ("Janko Kral");
+            Lotr.SetPages (352);
+            Lotr.SetCategory ("Romantic");
+            Lotr.SetAutor ("Marina");
+            Lotr.SetReleaseDate (2004);
+            Lotr.WriteParameters();
         }
         class Book
         {
             private string title;
             private int pages;
             private string category;
-            private string author;
+            private string autor;
             private int releaseDate;
 
-            public void Title(string newTitle) { title = newTitle; }
-            public void Pages(int newPages) { pages = newPages; }
-            public void Category(string newCategory) { category = newCategory; }
-            public void Autor(string newAutor) { autor = newAuthor; }
-            public void ReleaseDate(int newReleaseDate) { releaseDate = newReleaseDate; }
-            private int pages;
-        }
-        public void Write()
-        {
-            Console.WriteLine(title);
-            Console.WriteLine(pages);
-            Console.WriteLine(category);
-            Console.WriteLine(author);
-            Console.WriteLine(releaseDate);
-        }
+            public void SetTitle(string newTitle) { title = newTitle; }
+            public void SetPages(int newPages) { pages = newPages; }
+            public void SetCategory(string newCategory) { category = newCategory; }
+            public void SetAutor(string newAutor) { autor = newAutor; }
+            public void SetReleaseDate(int newReleaseDate) { releaseDate = newReleaseDate; }
+            public void WriteParameters()
+            {
+                Console.WriteLine(title);
+                Console.WriteLine(pages);
+                Console.WriteLine(category);
+                Console.WriteLine(autor);
+                Console.WriteLine(releaseDate);
+            }
 
+        }
+       
     }
 }
