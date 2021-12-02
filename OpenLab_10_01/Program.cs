9dea746c9b72a194350e522d0ff52a4cd282cee5
stark501 <mkudlacej@gmail.com>
2021-12-02 20:21:47 +0100

    OpenLab10.03

--- OpenLab_10_01/Program.cs
+++ OpenLab_10_01/Program.cs
@@ -17,14 +17,53 @@ namespace OpenLab_10_01
         }
         class Book
         {
-            private string title;
+            private string title
+            { get; set; }
             private int pages;
-            private string category;
-            private string autor;
+            public int Pages
+            {
+                get
+                {
+                    return pages;
+                }
+                set
+                {
+                    if (value < 0)
+                    {
+                        pages = 1;
+                    }
+                    else
+                    {
+                        pages = value;
+                    }
+                }
+            }
+            private string category
+            { get; set; }
+            private string autor
+            { get; set; }
             private int releaseDate;
+            public int ReleaseDate
+            {
+                get
+                {
+                    return releaseDate;
+                }
+                set
+                {
+                    if (value < 1450 && value > 2021)
+                    {
+                        releaseDate = -1;
+                    }
+                    else
+                    {
+                        releaseDate = value;
+                    }
+                }
+            }
 
             public void SetTitle(string newTitle) { title = newTitle; }
-            public void SetPages(int newPages) { pages = newPages; }
+        public void SetPages(int newPages) { pages = newPages; }
             public void SetCategory(string newCategory) { category = newCategory; }
             public void SetAutor(string newAutor) { autor = newAutor; }
             public void SetReleaseDate(int newReleaseDate) { releaseDate = newReleaseDate; }
